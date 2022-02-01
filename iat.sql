490126522c1e7ace5e1c375d31a25a00e088976c
Michael Janda <michael@iatsoftware.net>
2022-02-01 01:59:19 -0500

    Updated 'iat.sql' and added sample nginx configuration

--- iat.sql
+++ iat.sql
@@ -744,3 +744,6 @@ CREATE TABLE `users` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2022-02-01  1:49:34
+
+create user 'iat'@'localhost' identified by 'ezasabcdefg';
+grant all on iatserver_db.* to 'iat'@'localhost';
